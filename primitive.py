64959306b052b0ca6b6034bb688435f145290522
mrasilbek19 <asilbek190303@gmail.com>
2026-05-09 14:00:06 +0900

    feat: Learn stirng in Python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("=======================================")
+print("================= NUBMERS ===================")
 
 # in JAVA, variable is a name of storage location
 # in Python, variable is named reference because all of them are objects
@@ -13,3 +13,23 @@ result1 = count.bit_count()  # methodi bor
 result2 = count.numerator  # state
 
 print(result1, result2)
+
+print("=============== STRING =================")
+
+# METHODS: upper(), lower(), title(), find(), replace()
+
+course = "Mern AI fullStack"
+
+result = type(course)
+print(f"the type of course is: {result}")
+
+result = course.title()
+print(f"result 2 is: {result}")
+
+result = course.upper()
+print(f"result 3 is: {result}")
+
+result = course.replace("Mern", "Python")
+print(f"result 4 is: {result}")
+
+# print(course)
