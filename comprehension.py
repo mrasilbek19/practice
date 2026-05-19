589b0014d594860e941144eddbf23f24d029f163
mrasilbek19 <asilbek190303@gmail.com>
2026-05-19 11:08:54 +0900

    feat: Study comprehension and list comprehensions

---
+++ comprehension.py
@@ -0,0 +1,39 @@
+''' Comprehension
+    (1) What is comprehension & list comprehension
+    (2) set and Dictionary comprehension
+'''
+
+print("===== What is comprehension & list comprehension =====")
+# Comprehenson act like spread operator!
+
+''' Comprehension general syntax
+    a) *iterable
+    b) <expression> for item in iterable
+    c) <expression> for item in iterable <condition>
+'''
+
+numbers = [1, 2, 3, 5, 8, 9]
+list_numbers = [*numbers]  # a version
+
+print("list_numbers:", list_numbers)
+print(numbers is list_numbers)
+print(id(numbers), id(list_numbers))
+
+
+print("----------")
+people = [("Robert", 20), ("Bob", 24), ("Alex", 22)]
+list_people = [person[0] for person in people]
+print(list_people)
+
+print("----------")
+
+cars = [
+    ("Ferrari", 78),
+    ("Tayota", 87),
+    ("Audi", 116),
+    ("BMw", 109),
+    ("Pagani", 33)
+]
+
+list_cars = [car[0] for car in cars if car[1] > 80]
+print(list_cars)
